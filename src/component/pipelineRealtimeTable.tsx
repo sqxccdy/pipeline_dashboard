11545ea6b196c69572295c523a390725e0dcb6e6
Mason <sqxccdy@icloud.com>
2025-12-10 12:51:48 +0800

    添加redux支持，编写基础store文件

--- src/component/pipelineRealtimeTable.tsx
+++ src/component/pipelineRealtimeTable.tsx
@@ -1,66 +1,30 @@
 import {Component} from "react";
 import {Button, Table} from "antd";
-import { incremented, decremented } from "../features/pipelineSlice";
+import {initData, incremented, type DataItem, type TableColumn} from "../features/pipelineSlice";
 import {connect} from "react-redux";
 import type {RootState} from "../store.ts";
 
 interface  PipelineRealTimeTableProps{
-    counterValue: number;
+    dataSource: DataItem[];
+    columns: TableColumn[];
+    initData: () => void;
     incremented: () => void;
-    decremented: () => void;
 }
 
 class PipelineRealTimeTable extends Component<PipelineRealTimeTableProps>{
-    state = {
-        dataSource: []
-    };
-
-    columns = [
-        {
-            title: '姓名',
-            dataIndex: 'name',
-            key: 'name',
-        },
-        {
-            title: '年龄',
-            dataIndex: 'age',
-            key: 'age',
-        },
-        {
-            title: '住址',
-            dataIndex: 'address',
-            key: 'address',
-        },
-    ];
-
     componentDidMount() {
-        // 生成模拟数据
-        const dataSource = [];
-        for (let i = 0; i < 100; i++) {
-            dataSource.push({
-                key: i, // 确保每条数据有唯一key
-                name: `Edward King ${i}`,
-                age: i,
-                address: `London, Park Lane no. ${i}`,
-            });
-        }
-
-        this.setState({ dataSource });
+        this.props.initData();
     }
     handleAdd = ()=>{
         this.props.incremented();
     }
     render() {
-        const { dataSource } = this.state;
-        const { counterValue } = this.props;
-
         return (
             <>
-                {counterValue}
                 <Button onClick={this.handleAdd.bind(this)}>添加</Button>
                 <Table
-                    dataSource={dataSource}
-                    columns={this.columns}
+                    dataSource={this.props.dataSource}
+                    columns={this.props.columns}
                     rowKey="key"
                     pagination={false}
                     scroll={{ y: '80vh' }}
@@ -71,10 +35,11 @@ class PipelineRealTimeTable extends Component<PipelineRealTimeTableProps>{
     }
 }
 const mapStateToProps = (state: RootState) => ({
-    counterValue: state.counter.value,
+    dataSource: state.counter.dataSource,
+    columns: state.counter.columns,
 });
-
-export default connect(mapStateToProps, {
+const mapActionToProps = {
+    initData,
     incremented,
-    decremented,
-})(PipelineRealTimeTable);
+};
+export default connect(mapStateToProps,mapActionToProps)(PipelineRealTimeTable);
--- src/features/pipelineSlice.ts
+++ src/features/pipelineSlice.ts
@@ -1,23 +1,71 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+export interface DataItem {
+    key: number;
+    name: string;
+    age: number;
+    address: string;
+}
+
+export interface TableColumn {
+    title: string;
+    dataIndex: keyof DataItem; // 确保dataIndex是DataItem的键
+    key: string;
+}
+
+interface CounterState {
+    dataSource: DataItem[];
+    columns: TableColumn[];
+}
+
+const initialState: CounterState = {
+    dataSource: [],
+    columns: [
+        {
+            title: '姓名',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: '年龄',
+            dataIndex: 'age',
+            key: 'age',
+        },
+        {
+            title: '住址',
+            dataIndex: 'address',
+            key: 'address',
+        },
+    ]
+};
+
+
 const counterSlice = createSlice({
     name: 'counter',
-    initialState: {
-        value: 0
-    },
+    initialState,
     reducers: {
-        incremented: state => {
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the Immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
-            state.value += 1
+        initData(state){
+            const dataSource = [];
+            for (let i = 0; i < 100; i++) {
+                dataSource.push({
+                    key: i, // 确保每条数据有唯一key
+                    name: `Edward King ${i}`,
+                    age: i,
+                    address: `London, Park Lane no. ${i}`,
+                });
+            }
+            state.dataSource = dataSource;
         },
-        decremented: state => {
-            state.value -= 1
+        incremented: state => {
+            state.dataSource.splice(0, 0, {
+                key: 100, // 确保每条数据有唯一key
+                name: `Edward King 100`,
+                age: 100,
+                address: `London, Park Lane no. 100`,
+            })
         }
     }
 })
 
-export const { incremented, decremented } = counterSlice.actions
+export const { initData, incremented } = counterSlice.actions
 export default counterSlice.reducer;
